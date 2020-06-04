afacdf6b2fcdbe5b2e5d2cc59d424bb478df4e8b
huhn511 <sehe89@gmail.com>
2020-06-04 23:48:01 +0200

    :green_heart: just copy book content to dist

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ mkdir ./dist
 echo "Build 01_de_hallo_welt..."
 mdbook build ./01_de_hallo_welt
 mkdir ./dist/01_de_hallo_welt
-cp -rf ./01_de_hallo_welt/book ./dist/01_de_hallo_welt
+cp -r ./01_de_hallo_welt/book/. ./dist/01_de_hallo_welt
 echo "... 01_de_hallo_welt build finshed."
 echo "Build 01_en_hello_world..."
 mdbook build ./01_en_hello_world
 mkdir ./dist/01_en_hello_world
-cp -rf ./01_en_hello_world/book ./dist/01_en_hello_world
+cp -r ./01_en_hello_world/book/. ./dist/01_en_hello_world
 echo "... 01_en_hello_world build finshed."
 echo "Build index..."
 mdbook build
-cp -rf ./book ./dist
+cp -r ./book/. ./dist
 echo "... index build finshed."
