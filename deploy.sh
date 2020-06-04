2bd70effee8466c076d96b6804c498d6f3792c1f
huhn511 <sehe89@gmail.com>
2020-06-04 23:35:45 +0200

    :rocket: add index to deploy and create dist folder

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 # This is a comment!
-cd 01_de_hallo_welt    
+rm -f ./dist
+echo "Build 01_de_hallo_welt..."
+mdbook build ./01_de_hallo_welt
+mkdir ./dist/01_de_hallo_welt
+cp -rf ./01_de_hallo_welt/book ./dist/01_de_hallo_welt
+echo "... 01_de_hallo_welt build finshed."
+echo "Build 01_en_hello_world..."
+mdbook build ./01_en_hello_world
+mkdir ./dist/01_en_hello_world
+cp -rf ./01_en_hello_world/book ./dist/01_en_hello_world
+echo "... 01_en_hello_world build finshed."
+echo "Build index..."
 mdbook build
-cd ..
-cd 01_en_hello_world    
-mdbook build
+cp -rf ./book ./dist
+echo "... index build finshed."
