1dc881aa1b1f19b03b8feb12892b06ce5140efa7
huhn511 <sehe89@gmail.com>
2020-06-07 23:03:49 +0200

    :rocket: add deploy script for 02_de_iota_in_der_industrie

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,11 @@ mdbook build ./01_en_hello_world
 mkdir ./dist/01_en_hello_world
 cp -r ./01_en_hello_world/book/. ./dist/01_en_hello_world
 echo "... 01_en_hello_world build finshed."
+echo "Build 02_de_iota_in_der_industrie..."
+mdbook build ./02_de_iota_in_der_industrie
+mkdir ./dist/02_de_iota_in_der_industrie
+cp -r ./02_de_iota_in_der_industrie/book/. ./dist/02_de_iota_in_der_industrie
+echo "... 02_de_iota_in_der_industrie build finshed."
 echo "Build index..."
 mdbook build
 cp -r ./book/. ./dist
